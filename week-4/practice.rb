9d8d3e0333b74dc3d5cda2d06de63923bbed20bc
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2015-09-08 12:22:20 -0700

    Add defining-variables.rb and nums-letter.md

---
+++ week-4/practice.rb
@@ -0,0 +1,14 @@
+hours = 24
+year = 365
+
+yearhours = hours * year
+
+puts 'Hours in a Year: ', yearhours
+
+minutes = 60
+
+daymins = minutes * hours
+
+decmins = daymins * (year*10)
+
+puts "Minutes in a Decade: ", decmins
