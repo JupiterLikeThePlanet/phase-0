6a8cfa09765cc20696a9874f19ea54f0c524eb2c
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2015-09-09 08:48:14 -0700

    Add Release 4.3 files and directories

---
+++ week-4/address/my_solution.rb
@@ -0,0 +1,10 @@
+def make_address(street, city, state, zip)
+  puts "You live at " + street + ", in the beautiful city of "+ city + ", " + state + ". Your zip is " + zip.to_s + "."
+end
+
+puts make_address("633 Folsom St.", "San Francisco", "CA", "94107")
+
+# I worked on this challenge [by myself].
+
+
+# Your Solution Below
