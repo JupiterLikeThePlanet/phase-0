429b9a8234c62735b86fd4de91a349f79ec1ae91
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2015-09-16 12:18:13 -0700

    Add reflection to mode file

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -13,8 +13,18 @@
 # 0. Pseudocode
 
 # What is the input?
+# An array
+
 # What is the output? (i.e. What should the code return?)
+# A key of the most frequently occurring key
+
 # What are the steps needed to solve the problem?
+# Set a counter for each list item
+# loop through entire list, and increment each counter by one when it appears
+# display list item with the largest counter value
+
+
+
 
 =begin
 # 1. Initial Solution
@@ -44,5 +54,31 @@ end
 
 
 
-
+=begin
 # 4. Reflection
+(1) Which data structure did you and your pair decide to implement and why?
+
+We chose a hash because in order to determine the most frequently occuring value in the array, we must create a key/value pair that counts the amount of times a key occurs
+
+
+(2) Were you more successful breaking this problem down into implementable pseudocode than the last with a pair?
+
+There was a general pseudocode that gave an idea, but given the process we went through to get to a solution, there were some gaps.
+
+(3) What issues/successes did you run into when translating your pseudocode to code?
+
+Not accounting for how to extract the mode with just the key and that there could be multiple keys to return.
+
+
+(4) What methods did you use to iterate through the content? Did you find any good ones when you were refactoring? Were they difficult to implement?
+
+Each new method learned took time to understand and break down what exactly it was doing.  These are the methods used:
+
+.select
+.each_by_object
+.max_by
+.keys
+
+
+
+=end
