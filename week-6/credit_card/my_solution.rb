29959e5713d7782bbfcac8ac50a084bd5fb9c5d1
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2015-09-25 13:31:11 -0700

    Complete Credit Card Solution

--- week-6/credit_card/my_solution.rb
+++ week-6/credit_card/my_solution.rb
@@ -19,7 +19,10 @@
 
 # Don't forget to check on initialization for a card length
 # of exactly 16 digits
-
+###########################################################
+######## ATTEMPT ONE ######################################
+###########################################################
+=begin
 card_number = 1234567891234567
 
 class CreditCard
@@ -42,12 +45,42 @@ class CreditCard
        end
     end
     end
-    #["1", "2", "3", "4", "5", "6", "7", "8", "9", "1", "2", "3", "4", "5", "6", "7"]
+
 
 end
     card = CreditCard.new (card_number)
     puts "Check Card: ", card.check_card
+=end
+###########################################################
+######## ATTEMPT TWO ######################################
+###########################################################
 
+
+class CreditCard
+
+  def initialize(card_number)
+    raise ArgumentError.new("Invalid Card Number") unless card_number.kind_of?(Integer) && card_number.to_s.length == 16
+      @card_number = card_number
+  end
+
+  def check_card
+     @strings_in_arr = @card_number.to_s.split(//)
+     @integers_in_arr = @strings_in_arr.map {|element| element.to_i}
+     @unsummed_arr = @integers_in_arr.each_with_index.map {|x, i| i.even? ? (x*2) : x}
+     # Note explaining the block: if_this_is_a_true_value ? then_the_result_is_this : else_it_is_this
+     @large_int = @unsummed_arr.join
+
+     @indiv_str_arr = @large_int.split(//)
+     @indiv_int_arry = @indiv_str_arr.map {|element| element.to_i}
+     @final_sum = @indiv_int_arry.inject {|sum, n| sum + n}
+     # Note: Inject is a binary operation aka A binary operation is simply a rule for combining two objects of a given type, to obtain another object of that type. Inject can take an argument, which would become sum. However, in the case inject does not take an argument, sum is initially equal to zero, then gradually becomes a summation of all elements found in the enumerable.
+     # 74
+     @final_sum % 10 == 0
+  end
+end
+
+#card_number = 1234567891234567
+#card = CreditCard.new(card_number)
 # Refactored Solution
 
 
