4f823690ff5acea935199d112a4e51598abca817
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2015-09-30 22:31:43 -0700

    Add variables-objects.js & javascript_olympics.js

---
+++ week-7/javascript_olympics.js
@@ -0,0 +1,84 @@
+// Warm Up
+
+
+
+// Bulk Up
+
+// function addWin(athletes) {
+//   var txt = "";
+//   var sarah = {
+//     name: "Sarah Hughes",
+//     event: "Ladies' Singles",
+//   }
+//   var athlete = [sarah];
+//   var x;
+// //   for (x in athlete) {
+// //     return athlete.win = true;
+// //  }
+
+//   for (i=0;i<athlete.length;i++) {
+//      athlete[i].win = "she won";
+//   }
+// };
+// var athlete = [{name:"Sarah Hughes", event:"Ladies' Singles"}];
+// console.log(athlete.addWin)
+
+ var sarah = {
+    name: "Sarah Hughes",
+    event: "Ladies' Singles",
+  }
+
+ var athletes = [sarah];
+ for (var i=0;i<athletes.length;i++) {
+     athletes[i].win = "she won";
+  }
+
+console.log(sarah.win)
+
+
+
+// // Jumble your words
+// function jumble(words) {
+//   return words.split('').reverse().join('')
+// }
+
+// console.log(jumble("words"))
+
+// 2,4,6,8
+// function returnEven(numbers){
+//   var evens = [];
+// // for (i in array) {
+//   for (var i=0; i<numbers.length; i++) {
+//     if (numbers[i]%2 === 0)
+//       evens.push(numbers[i])
+//   }
+//   console.log(evens)
+// }
+
+//  returnEven([123,25,6,52,3,56,54,23])
+
+// // "We built this city"
+// function Athlete(name, age, sport, quote) {
+//   this.name = name;
+//   this.age = age;
+//   this.sport = sport;
+//   this.quote = quote;
+// }
+
+
+// //Driver code
+// var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
+// console.log(michaelPhelps.constructor === Athlete)
+// console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
+
+
+/* Reflection
+What JavaScript knowledge did you solidify in this challenge?
+
+
+What are constructor functions?
+
+
+How are constructors different from Ruby classes (in your research)?
+*/
+
